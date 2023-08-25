2b06d150420e0f5432c9a72e9c7488f99304a994
sanchi <126137666+sanchi789@users.noreply.github.com>
2023-08-25 11:16:05 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,15 +1,17 @@
-from flask import Flask, jsonify, request
+# from flask import Flask, jsonify, request
 
-app = Flask(__name__)
+# app = Flask(__name__)
 
-@app.route('/hello', methods=['GET'])
-def hello():
-    name = request.args.get('name')
-    if name:
-        message = f"Hello, {name}!"
-    else:
-        message = "Hello, World!"
-    return jsonify({'message': message})
+# @app.route('/hello', methods=['GET'])
+# def hello():
+#     name = request.args.get('name')
+#     if name:
+#         message = f"Hello, {name}!"
+#     else:
+#         message = "Hello, World!"
+#     return jsonify({'message': message})
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+# if __name__ == '__main__':
+#     app.run(host='0.0.0.0', port=5000)
+# This program prints Hello, world!
+print('Hello, world!')
